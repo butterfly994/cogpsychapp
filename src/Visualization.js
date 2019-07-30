dc18e713f6b650e1a8eef9be4384d08c05d5a33f
Yi Xie <adabelle.x@gmail.com>
2019-07-30 17:05:58 -0400

    Added different data point symbols, refactored Visualization component, added description section to Visualizations page

--- src/Visualization.js
+++ src/Visualization.js
@@ -2,39 +2,49 @@ import React from 'react'
 import * as d3 from 'd3'
 
 class Visualization extends React.Component {
-    componentDidMount(){
+    componentDidMount() {
         // 2. Use the margin convention practice 
-        var margin = {top: 100, right:100, bottom: 150, left: 100}
-        , width = window.innerWidth/2.5 - margin.left - margin.right // Use the window's width 
-        , height = window.innerHeight/2.5 - margin.top - margin.bottom; // Use the window's height
+        var margin = {
+                top: 100,
+                right: 100,
+                bottom: 150,
+                left: 100
+            },
+            width = window.innerWidth / 2 - margin.left - margin.right // Use the window's width 
+            ,
+            height = window.innerHeight / 2 - margin.top - margin.bottom; // Use the window's height
 
         // 5. X scale will use the index of our data
         var xScale = d3.scaleLinear()
-            .domain([0, this.props.n-1]) // input
+            .domain([this.props.xMin, this.props.xMax]) // input
             .range([0, width]); // output
 
         // 6. Y scale will use the randomly generate number 
         var yScale = d3.scaleLinear()
-            .domain([0, 1]) // input 
+            .domain([this.props.yMin, this.props.yMax]) // input 
             .range([height, 0]); // output 
 
         // 7. d3's line generator
         var line = d3.line()
-            .x(function(d, i) { return xScale(i); }) // set the x values for the line generator
-            .y(function(d) { return yScale(d.y); }) // set the y values for the line generator 
-            .curve(d3.curveMonotoneX) // apply smoothing to the line
+            .x(function (d) {
+                return xScale(d.x);
+            }) // set the x values for the line generator
+            .y(function (d) {
+                return yScale(d.y);
+            }) // set the y values for the line generator 
 
 
         // 1. Add the SVG to the page and employ #2
         var svg = d3.select('.' + this.props.name).append('svg')
             .attr('width', width + margin.left + margin.right)
             .attr('height', height + margin.top + margin.bottom)
-        .append('g')
+            .append('g')
             .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
+        //add title
         svg.append('text')
             .attr('transform',
-                    'translate(' + (width/2) + ' ,' + (0 - margin.top / 2) + ')')
+                'translate(' + (width / 2) + ' ,' + (0 - margin.top / 2) + ')')
             .style('text-anchor', 'middle')
             .style('font-size', '18pt')
             .text(this.props.title)
@@ -43,28 +53,36 @@ class Visualization extends React.Component {
         svg.append('g')
             .attr('transform', 'translate(0,' + height + ')')
             .call(d3.axisBottom(xScale)); // Create an axis component with d3.axisBottom
-        
+
         // text label for the x axis
-        svg.append('text')             
+        svg.append('text')
             .attr('transform',
-                    'translate(' + (width/2) + ' ,' + 
-                                (height + 50) + ')')
+                'translate(' + (width / 2) + ' ,' +
+                (height + 50) + ')')
             .style('text-anchor', 'middle')
             .style('font-size', '14pt')
             .text(this.props.xAxis);
+        
+        // description for the graph
+        svg.append('text')
+            .attr('transform',
+                'translate(50, ' + (height + margin.top) + ')')
+            .style('font-size', '12pt')
+            .text(this.props.description)
 
         // 4. Call the y axis in a group tag
         svg.append('g')
             .call(d3.axisLeft(yScale)); // Create an axis component with d3.axisLeft
 
+        // text label for the y axis
         svg.append('text')
             .attr('transform', 'rotate(-90)')
-            .attr('y', 0 - margin.left)
-            .attr('x',0 - (height / 2))
+            .attr('y', 0 - margin.left/1.5)
+            .attr('x', 0 - (height / 2))
             .attr('dy', '1em')
             .style('text-anchor', 'middle')
             .style('font-size', '14pt')
-            .text(this.props.yAxis);  
+            .text(this.props.yAxis);
 
         // 9. Append the path, bind the data, and call the line generator 
         svg.append('path')
@@ -75,18 +93,26 @@ class Visualization extends React.Component {
             .attr('d', line); // 11. Calls the line generator 
 
         // 12. Appends a circle for each datapoint 
+        //triangle = 5, circle = 0, square = 3, cross = 1, x same index as cross, but have to add 'rotate(-45)' to its transform
         svg.selectAll('.dot')
             .data(this.props.dataset)
-        .enter().append('circle') // Uses the enter().append() method
-            .attr('fill', 'navy')
+            .enter().append('path')
             .attr('stroke', '#fff')
-            .attr('cx', function(d, i) { return xScale(i) })
-            .attr('cy', function(d) { return yScale(d.y) })
-            .attr('r', 5)
+            .attr('fill', '#ffab00')
+            .attr('d', d3.symbol().type(function (d) {
+                return d3.symbols[d.pointType % 5];
+            }))
+            .attr('transform', function (d) {
+                if (d.pointType > 5) {
+                    return 'translate(' + xScale(d.x) + ',' + yScale(d.y) + ') rotate(-45)'
+                } else {
+                    return 'translate(' + xScale(d.x) + ',' + yScale(d.y) + ')'
+                }
+            })
     }
 
     render() {
-        return (
+        return ( 
             <div className = {this.props.name} style = {{display: 'inline-block'}}></div>
         )
     }
--- src/Visualizations.js
+++ src/Visualizations.js
@@ -7,20 +7,44 @@ class Visualizations extends React.Component {
     render() {
         let n = 21
         console.log(d3.range(n))
-        let dataset1 = d3.range(n).map(function(d) { return {'y': d3.randomUniform(1)() } })
+        let dataset1 = d3.range(n).map(function(d) { return {'y': d3.randomUniform(1)(), 'x': d, 'pointType': 6} })
         console.log(dataset1)
-        let dataset2 = d3.range(n).map(function(d) { return {'y': d3.randomUniform(1)() } })
+        let dataset2 = d3.range(n).map(function(d) { return {'y': d3.randomUniform(1)(), 'x': d, 'pointType': 3 } })
         return (
-            <div className = 'visCont'>
-                <div className = 'visCol'>
-                    <Visualization title = {'Bitcoin Price over Time'} xAxis = {'Days'} yAxis = {'Price in Dollars'} dataset = {dataset1} n = {21} name = {'Vis1'}></Visualization>
-                    <Visualization title = {'Bitcoin Price over Time'} xAxis = {'Days'} yAxis = {'Price in Dollars'} dataset = {dataset1} n = {21} name = {'Vis3'}></Visualization>
-                    <Visualization title = {'Bitcoin Price over Time'} xAxis = {'Days'} yAxis = {'Price in Dollars'} dataset = {dataset1} n = {21} name = {'Vis5'}></Visualization>
+            <div>
+                <div className = 'paragraph'>
+                    The code for symbols: x - inputs with letters all on one line, 
+                    + - input with six letters closely spaced, o - 2 x 3, 2 x 4, 2 x 5 
+                    grids, square - 3 x 3 grid, triangle - 2 x 4, 3 x 4 grids that mixed 
+                    letters and numbers. 
                 </div>
-                <div className = 'visCol'>
-                    <Visualization title = {'Temperature Over Time'} xAxis = {'Days'} yAxis = {'Temperature in Fahrenheit'} dataset = {dataset2} n = {21} name = {'Vis2'}></Visualization>
-                    <Visualization title = {'Temperature Over Time'} xAxis = {'Days'} yAxis = {'Temperature in Fahrenheit'} dataset = {dataset2} n = {21} name = {'Vis4'}></Visualization>
-                    <Visualization title = {'Temperature Over Time'} xAxis = {'Days'} yAxis = {'Temperature in Fahrenheit'} dataset = {dataset2} n = {21} name = {'Vis6'}></Visualization>
+                <div className = 'visCont'>
+                    <div className = 'visCol'>
+                        <Visualization 
+                            title = {'Bitcoin Price over Time'} 
+                            xAxis = {'Days'} 
+                            yAxis = {'Price in Dollars'} 
+                            dataset = {dataset1} 
+                            xMin = {0}
+                            xMax = {21}
+                            yMin = {0}
+                            yMax = {1}
+                            name = {'Vis1'}
+                            description = {'This is a description of a graph this is a description of a graph'}/>
+                    </div>
+                    <div className = 'visCol'>
+                        <Visualization 
+                            title = {'Temperature Over Time'} 
+                            xAxis = {'Days'} 
+                            yAxis = {'Temperature in Fahrenheit'} 
+                            dataset = {dataset2} 
+                            xMin = {0}
+                            xMax = {21}
+                            yMin = {0}
+                            yMax = {1}
+                            name = {'Vis2'}
+                            description = {'This is a description of a graph this is a description of a graph'}/>
+                    </div>
                 </div>
             </div>
         )
